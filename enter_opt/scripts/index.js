dbbcf7153d0c251c3f9fe1dfa2d59a420d35d53b
André Souza <andresouza@meraki.dev.br>
2022-06-01 21:46:52 -0300

    Enter otp.

---
+++ enter_opt/scripts/index.js
@@ -0,0 +1,51 @@
+const inputs = document.querySelectorAll(".opt-field input");
+
+inputs.forEach((input, index) => {
+  input.dataset.index = index;
+  input.addEventListener("paste", handleOnPasteOtp);
+  input.addEventListener("keyup", handleOtp);
+});
+
+
+function handleOnPasteOtp(e) {
+  const data = e.clipbordData.getData("text");
+  const value = data.split("");
+
+  if (value.length === inputs.length) {
+    inputs.forEach((input, index) => (input.value = value[index]));
+    submit();
+  }
+}
+
+function handleOtp(e) {
+  const input = e.target;
+  let value = input.value;
+  input.value = "";
+  input.value = value ? value[0] : "";
+  let fieldIndex = input.dataset.index;
+  if (value.length > 0 && fieldIndex < inputs.length - 1) {
+    input.nextElementSibling.focus();
+  }
+  if (e.key === "Backspace" && fieldIndex > 0) {
+    input.previusElementSibling.focus();
+  }
+
+  if (fieldIndex == inputs.length - 1) {
+    submit();
+  }
+}
+
+function submit() {
+  console.log("Submitting...");
+
+  let otp = "";
+  inputs.forEach(input => {
+    otp += input.value;
+    input.disabled = true;
+    input.classList.add("disabled");
+  })
+  if (otp.length === 6) {
+    document.querySelector("#button_submit")
+      .disabled = false
+  }
+}
